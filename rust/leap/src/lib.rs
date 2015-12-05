c2665f3944a229dd4e4f3280e5681aa2e2d250c6
Andy Georges <itkovian@gmail.com>
2015-12-06 00:11:20 +0100

    rust leap with simpler guards

--- rust/leap/src/lib.rs
+++ rust/leap/src/lib.rs
@@ -1,13 +1,8 @@
 
 pub fn is_leap_year(year: i32) -> bool {
-    match year % 4 {
-        0 => match year % 100 {
-            0 => match year % 400 {
-                0 => true,
-                _ => false
-            },
-            _ => true
-        },
-        _ => false
+    match year {
+        y if y % 400 == 0 => true,
+        y if y % 100 == 0 => false,
+        y => y % 4 == 0,
     }
 }
