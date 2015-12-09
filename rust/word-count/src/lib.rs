2e5fe31591cb115eb578b64c43212005a6d7273f
Andy Georges <itkovian@gmail.com>
2015-12-09 23:24:52 +0100

    rust wordcount use fold to fill the HashMap

--- rust/word-count/src/lib.rs
+++ rust/word-count/src/lib.rs
@@ -3,17 +3,13 @@ use std::collections::HashMap;
 
 pub fn word_count(s: &str) -> HashMap<String, u32> {
 
-    let mut hm: HashMap<String, u32> = HashMap::new();
-    let words: Vec<String> = s.to_lowercase()
-                              .split(|c| ! char::is_alphanumeric(c))
-                              .filter(|w| ! w.is_empty())
-                              .map(|s| s.to_string())
-                              .collect();
+    s.to_lowercase()
+     .split(|c| ! char::is_alphanumeric(c))
+     .filter(|w| ! w.is_empty())
+     .map(|s| s.to_string())
+     .fold(HashMap::new(), |mut hm, word| {
+        *hm.entry(word).or_insert(0) += 1; // this looks odd to me, got it from http://stackoverflow.com/questions/31884309/how-to-fold-using-a-hashmap-as-an-accumulator
+        hm
+     })
 
-    for word in words.iter() {
-        let word_ = word.clone();
-        let count = hm.entry(word_).or_insert(0);
-        *count += 1;
-    }
-    return hm;
 }
